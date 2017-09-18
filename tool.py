79aa638a7515d550c8810e42ba23e6615ada5c64
Christiaan Quyn <christianq010@gmail.com>
2017-09-18 14:32:12 +0530

    refactor: Add query that executes and retrieves our results from the database and successfully queries questions 1
    Question1: Query top 3 results for popular articles and print out.

--- tool.py
+++ tool.py
@@ -1,32 +1,52 @@
 import psycopg2
 DB_NAME = "news"
 
+
+
+# Connect to our Database and Fetch_Results
+def fetch_results(query):
+    try: 
+        # Connect to our Database
+        db = psycopg2.connect(database=DB_NAME)
+        # cursor runs queries and fetches results
+        c = db.cursor()
+        # execute queries above from the cursor 
+        c.execute(query)
+        # fetch results from the cursor
+        query_results = c.fetchall()
+        db.close()
+        return query_results
+    except BaseException:
+        print ("Sorry, unable to fetch results from Database")
+
 # Question 1. What are the most popular three articles of all time?
-query1 = """ .. """
+def print_popular_articles():
+    # Query for Top 3 results
+    query1 = """
+            select title,count(log.id) as views 
+            from articles,log 
+            where log.path = CONCAT('/article/', articles.slug) 
+            group by articles.title 
+            order by views desc limit 3;
+            """
+    popular_articles = fetch_results(query1)
+    print """ The most popular three articles """
+    for result in popular_articles:
+        print '\n' + '"' + result[0] + '" -- ' + str(result[1]) + " views"
+    print(' ')
+
 
 # Question 2. Who are the most popular article authors of all time?
-query2 = """ ... """
+# def print_popular_authors():
+    
 
 # Question 3. On which days did more than 1% of requests lead to errors?
-query3 = " ..  "
+# def print_error_request():
+    
 
-# Connect to our Database
-db = psycopg2.connect(database=DB_NAME)
 
-# cursor runs queries and fetches results
-c = db.cursor()
 
-# execute queries above from the cursor 
-c.execute(query1)
 
-# fetch results from the cursor
-query_results = c.fetchall()
 
-db.close()
-
-# Create to function
-def print_query_results(query_result):
-    for result in query_results:
-        print ('\t' + str(result[0]) + ' : ' + str(result[1]))
-
+print_popular_articles()
 
