8c9248bc43c3a6d50be20ff4b9e7857099a9c045
Christiaan Quyn <christianq010@gmail.com>
2017-09-18 17:47:46 +0530

    feat: Successfully create result for question 2 refactor: Add Views to query 1 and simplify question 1 query, same view used in query 2 to fetch results successfully
    Question 2: Find most popular authors by views

--- tool.py
+++ tool.py
@@ -2,6 +2,26 @@ import psycopg2
 DB_NAME = "news"
 
 
+# Create the following Views before executing this file
+"""
+Top 3 Viewed articles:
+CREATE VIEW article_views AS
+SELECT title,count(log.id) AS views
+FROM articles,log
+WHERE log.path = CONCAT('/article/', articles.slug)
+GROUP BY articles.title
+ORDER BY views DESC;
+
+Most Popular Authors:
+CREATE VIEW authors_by_article AS
+SELECT title, name
+FROM articles, authors
+WHERE articles.author = authors.id;
+
+
+
+
+"""
 
 # Connect to our Database and Fetch_Results
 def fetch_results(query):
@@ -21,13 +41,9 @@ def fetch_results(query):
 
 # Question 1. What are the most popular three articles of all time?
 def print_popular_articles():
-    # Query for Top 3 results
     query1 = """
-            select title,count(log.id) as views 
-            from articles,log 
-            where log.path = CONCAT('/article/', articles.slug) 
-            group by articles.title 
-            order by views desc limit 3;
+            select title, views 
+            from article_views limit 3;
             """
     popular_articles = fetch_results(query1)
     print """ The most popular three articles """
@@ -37,8 +53,19 @@ def print_popular_articles():
 
 
 # Question 2. Who are the most popular article authors of all time?
-# def print_popular_authors():
-    
+def print_popular_authors():
+    query2 = """
+            SELECT name, sum(article_views.views) AS views
+            FROM authors_by_article, article_views
+            WHERE authors_by_article.title = article_views.title
+            GROUP BY name ORDER BY views DESC;
+            """
+    popular_authors = fetch_results(query2)
+    print """ The Most Popular Authors """
+    for result in popular_authors:
+        print '\n' + '"' + result[0] + '" -- ' + str(result[1]) + " views"
+    print(' ')
+
 
 # Question 3. On which days did more than 1% of requests lead to errors?
 # def print_error_request():
@@ -46,7 +73,7 @@ def print_popular_articles():
 
 
 
-
-
-print_popular_articles()
-
+# Run all 3 functions when executed
+if __name__ == "__main__":
+    print_popular_articles()
+    print_popular_authors()
