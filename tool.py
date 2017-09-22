f43db63ffbfee859de953e4a3e2b8037379d80ee
Christiaan Quyn <christianq010@gmail.com>
2017-09-22 21:46:32 +0530

    refactor: Format question 1 output string

--- tool.py
+++ tool.py
@@ -12,31 +12,33 @@ group by articles.title
 order by page_views desc limit 3;
 """
 def print_popular_articles():
-    print """ The Most Popular Articles """
+    print " The Most Popular Articles "
+    print '--'
     query1 = "select title, concat(concat(page_views,' '), 'views') as views from top_3_articles;"
     try:
         # Connect to our Database
-        db = psycopg2.connect(database="news")
+        database = psycopg2.connect(database="news")
         # cursor runs queries and fetches results
-        c = db.cursor()
+        c = database.cursor()
         # execute queries above from the cursor
         c.execute(query1)
         # fetch results from the cursor
         query_results = c.fetchall()
-        db.close()
+        database.close()
         for i in query_results:
             print ('"' + i[0] + '"'+ ' -- ' +  i[1])
-            print '--'
+        print '\n'
     except BaseException:
         print("Sorry, unable to fetch results from Database")
 
-"""
 
 # Question 2. Who are the most popular article authors of all time?
 def print_popular_authors():
-    print  The Most Popular Authors
+    print " The Most Popular Authors "
 
 
+
+"""
 # Question 3. On which days did more than 1% of requests lead to errors?
 def print_error_request():
     print Days in which more than 1% of requests lead to errors
