86d58d438c10ee6908a5261bffeb81c51d224ac5
Christiaan Quyn <christianq010@gmail.com>
2017-10-03 14:31:33 +0530

    refactor: Include the shebang line and remove commented views - sugested by reviewer
    shebang line - (python 2.7) #! /usr/bin/env python
    (python 3+)  #! /usr/bin/env python3

--- tool.py
+++ tool.py
@@ -1,28 +1,12 @@
+#! /usr/bin/env python
+
 import psycopg2
 from datetime import datetime
 
 
+# Create the Views as instructed on the README file first
+
 # Question 1. What are the most popular three articles of all time?
-# Create the following View first in your psql terminal
-"""
-Create view top_3_articles as
-select title, count(*) as page_views
-from articles join log
-on log.path = concat('/article/', articles.slug)
-where status !='404 NOT FOUND'
-group by articles.title
-order by page_views desc;
-"""
-
-"""
-* Exploring the database in the terminal we find
-slug in the articles table resemble the path in the log table.
-* We use the concat function and combine '/article/'
-into the slug to perform a join on the table.
-https://www.w3resource.com/PostgreSQL/concat-function.php
-"""
-
-
 def print_popular_articles():
     print " The Most Popular Articles "
     print '--'
@@ -49,24 +33,6 @@ def print_popular_articles():
 
 
 # Question 2. Who are the most popular article authors of all time?
-# Create the following View first in your psql terminal
-"""
-Create view top_authors as
-select author, count(*) as page_views
-from articles join log
-on log.path = concat('/article/', articles.slug)
-where status !='404 NOT FOUND'
-group by articles.author
-order by page_views desc;
-"""
-
-"""
-* Similar to the previous view
-but this time we return a table with the author to perform a
-join with the authors table.
-"""
-
-
 def print_popular_authors():
     print " The Most Popular Authors "
     print '--'
@@ -89,51 +55,6 @@ def print_popular_authors():
 
 
 # Question 3. On which days did more than 1% of requests lead to errors?
-"""
-Create view All_Requests2 as
-select time ::timestamp::date as date, count(*) as total_requests
-from log
-group by date
-order by total_requests desc;
-
-Create view All_Errors2 as
-select time ::timestamp::date as date, count(*) as requests_failures
-from log
-where status = '404 NOT FOUND'
-group by date
-order by requests_failures desc;
-
-Create view daily_error_number2 as
-select All_Errors2.date,
-cast(All_Errors2.requests_failures as decimal) /
-cast(All_Requests2.total_requests as decimal) as daily_error
-from All_Requests2 join All_Errors2
-on All_Requests2.date = All_Errors2.date
-order by daily_error desc;
-
-Create view daily_error_percentage_table as
-select date,
-round(100 * (daily_error), 2) as daily_error_percentage
-from daily_error_number2
-order by daily_error_percentage desc limit 5;
-"""
-
-"""
-* The idea behind these views, is that we create separate tables
-for total requests,total errors.
-* Then we divide the total errors by total requests.
-* I convert it's type to decimal as I ran into errors while rounding up and
-multiplying in the next table.
-https://stackoverflow.com/questions/42149496/pgsql-error-you-might-need-to-add-explicit-type-casts
-* We cast our timestamp to a date by suffixing it with ::date
-https://stackoverflow.com/questions/6133107/extract-date-yyyy-mm-dd-from-a-timestamp-in-postgresql
-* Used strftime = "string format time" for datetime conversion because
-I ran into an error printing the query results onto the terminal.
-https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
-http://strftime.org/
-"""
-
-
 def print_error_request():
     print " Days in which more than 1% of requests lead to errors "
     print '--'
