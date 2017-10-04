5e2840f9fcf61c0dee07c6869c387ac5d6d94757
Christiaan Quyn <christianq010@gmail.com>
2017-10-04 12:21:12 +0530

    refactor: Create reusable function for fetch and connecting to our DB, create function for printing our results for query 1 and 2

--- tool.py
+++ tool.py
@@ -4,6 +4,31 @@ import psycopg2
 from datetime import datetime
 
 
+# Connect to our database and fetch results
+def connect(query):
+    try:
+        # Connect to our Database
+        database = psycopg2.connect(database="news")
+        # cursor runs queries and fetches results
+        c = database.cursor()
+        # execute queries above from the cursor
+        c.execute(query)
+        # fetch results from the cursor
+        query_results = c.fetchall()
+        database.close()
+        return query_results
+    except BaseException:
+        print("Sorry, unable to fetch results from Database")
+
+# Function to Print Our Results
+def print_results(query_results):
+    # Iterate over the rows and get our results
+    for i in query_results:
+        print('"' + i[0] + '"' + ' -- ' + i[1])
+    print '\n'
+
+
+
 # Create the Views as instructed on the README file first
 
 # Question 1. What are the most popular three articles of all time?
@@ -14,23 +39,8 @@ def print_popular_articles():
             select title, concat(concat(page_views,' '), 'views')as views
             from top_3_articles limit 3;
             """
-    try:
-        # Connect to our Database
-        database = psycopg2.connect(database="news")
-        # cursor runs queries and fetches results
-        c = database.cursor()
-        # execute queries above from the cursor
-        c.execute(query1)
-        # fetch results from the cursor
-        query_results = c.fetchall()
-        database.close()
-        # Iterate over the rows and get our results
-        for i in query_results:
-            print('"' + i[0] + '"' + ' -- ' + i[1])
-        print '\n'
-    except BaseException:
-        print("Sorry, unable to fetch results from Database")
-
+    popular_articles = connect(query1)
+    print_results(popular_articles)
 
 # Question 2. Who are the most popular article authors of all time?
 def print_popular_authors():
@@ -41,18 +51,8 @@ def print_popular_authors():
             from top_authors join authors on top_authors.author = authors.id
             limit 4;
             """
-    try:
-        database = psycopg2.connect(database="news")
-        c = database.cursor()
-        c.execute(query2)
-        query_results = c.fetchall()
-        database.close()
-        for i in query_results:
-            print('"' + i[0] + '"' + ' -- ' + i[1])
-        print '\n'
-    except BaseException:
-        print("Sorry, unable to fetch results from Database")
-
+    popular_authors = connect(query2)
+    print_results(popular_authors)
 
 # Question 3. On which days did more than 1% of requests lead to errors?
 def print_error_request():
@@ -63,17 +63,10 @@ def print_error_request():
             as percentage
             from daily_error_percentage_table limit 1;
             """
-    try:
-        database = psycopg2.connect(database="news")
-        c = database.cursor()
-        c.execute(query3)
-        query_results = c.fetchall()
-        database.close()
-        for i in query_results:
-            print(i[0].strftime('%B %d, %Y') + ' -- ' + i[1])
-        print '\n'
-    except BaseException:
-        print("Sorry, unable to fetch results from Database")
+    error_request = connect(query3)
+    for i in error_request:
+        print(i[0].strftime('%B %d, %Y') + ' -- ' + i[1])
+    print '\n'
 
 
 # Run all 3 functions when executed
